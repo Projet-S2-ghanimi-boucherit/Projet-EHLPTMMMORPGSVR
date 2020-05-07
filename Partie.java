61e6809f6aa53a0c73a4f08205e1d4ae415bac10
Projet-S2-ghanimi-boucherit <62246157+Projet-S2-ghanimi-boucherit@users.noreply.github.com>
2020-05-07 18:18:33 +0200

    Joueur + Partie

--- Partie.java
+++ Partie.java
@@ -1,40 +1,134 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
+package projetTutore;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Partie {
-    @objid ("5474aa0d-0fe0-46f1-ac69-9921e1441343")
-    public String Nom;
+	public String Nom;
 
     public Personnage personnage;
-    public String numPartie;
-    
+    public int numPartie;
+
     public Partie(int UnNumPartie, Personnage unPersonnage) {
     	this.numPartie  = UnNumPartie;
     	this.personnage = unPersonnage;
-    	}
-
-    public void Sauvegarder() {
-    }
-
-
-    public void Charger() {
-    }
-
-
-    public void Creer() {
-    }
-
-
-    public void Supprimer() {
     }
     
-    public String getNom() {
-        return this.Nom;
-    }
+    
+    public static void main(String[] args) {
+    	Charger();
+		
+	}
+	
+	public static void Charger() {
+		
+		// initialisation de la variable qui va nous permettre de lire
+	    FileInputStream Lecture = null;
+
+	    try {
+	      // Ouvre un flux pointant sur le fichier
+	              Lecture = new FileInputStream("C://Users//Fareh//Desktop//Sauvegardes.txt");
+
+		while (true) {
+		  int valeurEntièreOctet = Lecture.read();
+		  System.out.print(" " + valeurEntièreOctet);
+			   
+		  if (valeurEntièreOctet  == -1) {
+	          // On a atteint la fin du fichier
+		    // On sort de la boucle
+		    break;
+	        }  
+		} // Fin de la boucle while
+	      // Lecture.close(); pas à cet endroit
+	    } catch (IOException exception) {
+		System.out.println("Impossible de lire le fichier : " + exception.toString());} 
+	 finally {
+	 	
+	    	try {
+	 		// On met Lecture.close ici pour pouvoir fermer le flux même en cas d'erreur
+		  Lecture.close();
+		} catch (Exception exception1){
+		  exception1.printStackTrace() ;
+		}
+		System.out.println("Lecture du fichier terminée.");	           
+	    
+	    }
+	
+	
+	}
+	
+		public static void Sauvegarder() {
+		
+		      int données[] = {56, 230, 123, 43, 11, 37};
+			  FileOutputStream monFichier = null;
+			  BufferedOutputStream tampon = null;
+
+			  try {
+			    monFichier = new FileOutputStream("C://Users//Fareh//Desktop//Sauvegardes.txt");
+			    // Connecte les flux
+			    tampon = new BufferedOutputStream(monFichier);
+			    for (int i = 0; i < données.length; i++) {
+				tampon.write(données[i]);
+				System.out.println("Partie sauvegardée");
+			    }
+			  } catch (IOException exception) { 
+			    exception.printStackTrace();
+			  } finally {  
+			    try {
+				tampon.flush();
+				tampon.close();
+				monFichier.close();
+			    } catch (IOException exception1) {
+				exception1.printStackTrace();
+			    	
+			  
+			    }
+			  }
+		}
+				
+		public static void Creer() {
+		   DataOutputStream Nouv = null;
+		try {
+			Nouv = new DataOutputStream(new FileOutputStream("C://Users//Fareh//Desktop//Sauvegardes.txt"));
+			System.out.println("La partie a été créee avec succès !");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+		   try {
+			Nouv.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		  	   
+			}
+		   
+	   	}
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+   
+   
 
 
-    public void setNom(String value) {
-        this.Nom = value;
-    }
-
-}
---
+++ Personnage.java
@@ -0,0 +1,270 @@
+package projetTutore;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class Personnage {
+	private Personnage perso;
+	private int val;
+    private int PointAction;
+    
+    
+    private int Force;
+    private int Adresse;
+    private int Endurance;
+    
+    private int Initiative;
+    private int Attaque;
+    private int Defense;
+    
+    private int PointDeVie;
+    private int Xp;
+    public ArrayList<Objet> SacObjet;
+
+    public Personnage() {
+    	System.out.println("Répartissez 18 degrés entre les trois caractéristiques dans cette ordre: Force, Adresse, Endurance");
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int force = sc.nextInt();
+    	this.Force = force;
+    	
+    	int adresse = sc.nextInt();
+    	this.Adresse = adresse;
+    	
+    	int endurance = sc.nextInt();
+    	this.Endurance = endurance;
+     	
+    	sc.close();
+    	
+    	this.PointAction = 10;
+    	this.Initiative = 0;
+    	this.Attaque = 0;
+    	this.Defense = 0;
+    	
+    	this.Xp = 0;
+    	this.PointDeVie = 100;
+    	
+    	SacObjet = new ArrayList<Objet>();
+    	
+    }
+
+    
+    // METHODES //
+    
+    public void Equiper(Objet obj){
+    	this.SacObjet.add(obj);
+    	
+    }
+    
+    public void Soin() {
+    	int val = this.getPointDeVIe()+5;
+    			  this.setPointDeVIe(val);
+    }
+
+    public void Molotov() {
+        int val = this.getPointDeVIe()-6;
+    			  this.setPointDeVIe(val); 
+    	
+    }
+    
+    // DEBUT CODE LUNDI 6 AVRIL
+
+    public boolean seDeplacer() {
+    	
+    	if (getPointAction() >= 2) {
+    		
+    		Scanner sc = new Scanner(System.in);
+    	    System.out.println("Saisissez la case où vous voulez vous déplacer : ");
+    	    
+    	    int position = sc.nextInt();
+    	    sc.close();
+    	        		
+    		int PA = getPointAction();
+    		int nouvPA = PA - 2;
+    		setPointAction(nouvPA);
+    		
+    		return true;
+    	}
+    	
+    	else {
+    		System.out.println("Vous n'avez pas assez de Point d'Action, veuiller choisir une autre action ou attendre d'avoir assez de point");
+    		return false;
+    	}
+    }
+    
+    
+    public boolean attaquer() {
+    	if (getPointAction() >= 3) {
+    		
+    		return true;
+    	}
+    	
+    	else {
+    		System.out.println("Vous n'avez pas assez de Point d'Action, veuiller choisir une autre action ou attendre d'avoir assez de point");
+    		return false;
+    	}
+    }
+    
+    public void useObject(Objet o) {
+    }
+    
+
+    public void ChoixAction() {
+    	System.out.println("Veuillez choisir une action parmis, Attaquer : (A), Déplacer : (B), Utiliser un objet : (C).");
+
+    			Scanner sc = new Scanner(System.in);
+    			String A = sc.nextLine();
+    			sc.close();
+    			
+    			A = "d";
+    }
+
+    
+    public boolean isAlpha(String name) {
+        return name.matches("[a-zA-Z]+");
+    }
+
+    public String AfficherPointDeVie() {
+    	
+    	if ( this.PointDeVie == 0 ) {
+    		
+    		String newline=System.getProperty("line.separator");
+    		return new String ("Points de vie :" + this.PointDeVie + newline +"Vous êtes mort..." + newline + "Fin de la partie");
+    	}
+    	else if ( this.PointDeVie >=79 ) {
+    		
+    		String newline=System.getProperty("line.separator");
+    		return new String ("Points de vie :" + this.PointDeVie + newline +"Votre niveau de blessure est nulle ou quasi nulle");
+    	}
+    	
+    	else if (this.PointDeVie >=59) {
+    		String newline=System.getProperty("line.separator");
+    		return new String ("Points de vie :" + this.PointDeVie + newline +"Votre niveau de blessure est superficielle");
+    	}
+    	
+    	else if (this.PointDeVie >=39) {
+    		String newline=System.getProperty("line.separator");
+    		return new String ("Points de vie :" + this.PointDeVie + newline +"Vous êtes légèrment blessé");
+    	}
+    	
+    	else if (this.PointDeVie >=19) {
+    		String newline=System.getProperty("line.separator");
+    		return new String ("Points de vie :" + this.PointDeVie + newline +"Vous êtes gravement blessé");
+    	}
+    	
+    	else {
+    		String newline=System.getProperty("line.separator");
+    		return new String ("Points de vie :" + this.PointDeVie + newline +"Vous êtes actuellement inconscient et ne pouvez réaliser aucune action à moins d'être soigné");
+    	}
+    }
+    
+    // FIN CODE LUNDI 6 AVRIL
+    
+    public String toString() {
+    	String newline=System.getProperty("line.separator");
+    	String s;
+    	s = "Force: " + this.Force + ", Adresse : " + this.Adresse + ", Endurance: " + this.Endurance + newline +"Initiative: " + this.Initiative + ", Attaque: " + this.Attaque + ", Defence: " + this.Defense + newline +"Point de vie: " + this.PointDeVie + ", Point d'action: " + this.PointAction + " Point d'experience(XP): " + this.Xp + newline +"Sac: " + this.SacObjet ;
+    	return s;
+    }
+    
+    
+    
+    // GETTER ET SETTER //
+    
+    public ArrayList<Objet> getSacObjet () {
+    	return this.SacObjet;
+    }
+
+    int getPointAction() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.PointAction;
+    }
+
+    void setPointAction(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.PointAction = value;
+    }
+
+    int getAdresse() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.Adresse;
+    }
+
+    void setAdresse(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.Adresse = value;
+    }
+
+    int getInitiative() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.Initiative;
+    }
+
+    void setInitiative(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.Initiative = value;
+    }
+
+    public int getForce() {
+		return Force;
+	}
+
+
+	public void setForce(int force) {
+		Force = force;
+	}
+
+
+	int getAttaque() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.Attaque;
+    }
+
+    void setAttaque(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.Attaque = value;
+    }
+
+    int getDefense() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.Defense;
+    }
+
+    void setDefense(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.Defense = value;
+    }
+
+    int getEndurance() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.Endurance;
+    }
+
+    void setEndurance(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.Endurance = value;
+    }
+
+    int getPointDeVIe() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.PointDeVie;
+    }
+
+    void setPointDeVIe(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.PointDeVie = value;
+    }
+
+
+    int getXp() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.Xp;
+    }
+
+    void setXp(int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.Xp = value;
+    }
+
+} 
