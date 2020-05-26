b1c6d85c3fc46ea7a88ca974dd73bdbcf9e9fff8
Projet-S2-ghanimi-boucherit <62246157+Projet-S2-ghanimi-boucherit@users.noreply.github.com>
2020-05-26 12:41:33 +0200

    Joueur + Partie

--- Partie.java
+++ Partie.java
@@ -1,4 +1,4 @@
-
+package projetTutore;
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -6,86 +6,72 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class Partie {
 	public String Nom;
+	public Cases cases;
+	 public Cases[][] Tabcases;
 
     public Personnage personnage;
     public int numPartie;
     public CarteDuJeu carte;
+    public Partie game;
 
     public Partie(int UnNumPartie, Personnage unPersonnage, CarteDuJeu uneCarte) {
-    	this.numPartie  = UnNumPartie;
+    	this.numPartie  = numPartie;
     	this.personnage = unPersonnage;
     	this.carte = uneCarte;
     }
 	
-	public static void Charger() {
+	public void Charger() {
+
+
+		    //lecture
+		    ArrayList<String> arraylist= new ArrayList<String>();
+		    try {
+		      FileInputStream fileIn = new FileInputStream("C://Users//Fareh//Desktop//Sauvegardes.txt");
+		      ObjectInputStream ois = new ObjectInputStream(fileIn);
+		      arraylist = (ArrayList) ois.readObject();
+		      ois.close();
+		      fileIn.close();
+		    } catch (FileNotFoundException e) {
+		      e.printStackTrace();
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    } catch (ClassNotFoundException e) {
+		      // TODO Auto-generated catch block
+		       e.printStackTrace();
+		    }
+
+		    System.out.println("Lire les données: \n");
+		    for(String o:arraylist)
+		      System.out.println(o);
+		  }
 		
-		// initialisation de la variable qui va nous permettre de lire
-	    FileInputStream Lecture = null;
-
-	    try {
-	      // Ouvre un flux pointant sur le fichier
-	              Lecture = new FileInputStream("C://Users//Fareh//Desktop//Sauvegardes.txt");
-
-		while (true) {
-		  int valeurEntièreOctet = Lecture.read();
-		  System.out.print(" " + valeurEntièreOctet);
-			   
-		  if (valeurEntièreOctet  == -1) {
-	          // On a atteint la fin du fichier
-		    // On sort de la boucle
-		    break;
-	        }  
-		} // Fin de la boucle while
-	      // Lecture.close(); pas à cet endroit
-	    } catch (IOException exception) {
-		System.out.println("Impossible de lire le fichier : " + exception.toString());} 
-	 finally {
-	 	
-	    	try {
-	 		// On met Lecture.close ici pour pouvoir fermer le flux même en cas d'erreur
-		  Lecture.close();
-		} catch (Exception exception1){
-		  exception1.printStackTrace() ;
-		}
-		System.out.println("Lecture du fichier terminée.");	           
-	    
-	    }
 	
 	
-	}
 	
-		public static void Sauvegarder() {
+		public  void Sauvegarder() {
 		
-		      int données[] = {56, 230, 123, 43, 11, 37};
-			  FileOutputStream monFichier = null;
-			  BufferedOutputStream tampon = null;
-
 			  try {
-			    monFichier = new FileOutputStream("C://Users//Fareh//Desktop//Sauvegardes.txt");
-			    // Connecte les flux
-			    tampon = new BufferedOutputStream(monFichier);
-			    for (int i = 0; i < données.length; i++) {
-				tampon.write(données[i]);
-				System.out.println("Partie sauvegardée");
-			    }
-			  } catch (IOException exception) { 
-			    exception.printStackTrace();
-			  } finally {  
-			    try {
-				tampon.flush();
-				tampon.close();
-				monFichier.close();
-			    } catch (IOException exception1) {
-				exception1.printStackTrace();
-			    	
-			  
-			    }
+			       FileOutputStream fileOut = new FileOutputStream("C://Users//Fareh//Desktop//Sauvegardes.txt");
+			       ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			       out.writeObject(this.carte);
+			       out.close();
+			       fileOut.close();
+			       System.out.println("\nSerialisation terminée avec succès...\n");
+			 
+			     } catch (FileNotFoundException e) {
+			       e.printStackTrace();
+			     } catch (IOException e) {
+			       e.printStackTrace();
+			     }
 			  }
-		}
-				
+			
+
 		public static void Creer() {
 		   DataOutputStream Nouv = null;
 		try {
@@ -108,7 +94,9 @@ public class Partie {
 	
 	
 	
-		public static void main(String[] args){
+	
+
+		public void main(String[] args){
 		Sauvegarder();
 		 Charger();
 		}
